aa671d21eaba3014af38a865123c66510e4cb34a
Jason <jhphan1@gmail.com>
2022-03-30 10:20:25 -0700

    Create editTodoPopup

--- src/popups.js
+++ src/popups.js
@@ -201,8 +201,130 @@ const addProjectPopup = (() => {
 
 const editTodoPopup = (() => {
     function displayEditTodo(e) {
-        console.log(this);
+        // Generate Add Todo Popup
+        const body = document.querySelector("body");
+
+        const editTodoPopup = document.createElement("div");
+        editTodoPopup.classList.add("todo-popup");
+
+        const header = document.createElement("div");
+        header.id = "edit-todo-header";
+        header.textContent = "Edit Task"
+        editTodoPopup.appendChild(header);
+
+        const content = document.createElement("form");
+        content.classList.add("todo-popup-content");
+        editTodoPopup.appendChild(content);
+
+        const title = document.createElement("input");
+        title.classList.add("input-todo-title");
+        title.placeholder = "Edit title";
+        title.maxlength = "30";
+        title.required = true;
+        content.appendChild(title);
+
+        const description = document.createElement("textarea");
+        description.classList.add("input-todo-description");
+        description.placeholder = "Edit description";
+        description.maxlength = "150";
+        description.cols = "50";
+        description.rows = "4";
+        content.appendChild(description);
+
+        const dateLabel = document.createElement("label");
+        dateLabel.for = "todo-date";
+        dateLabel.textContent = "Due date: ";
+        content.appendChild(dateLabel);
+
+        const date = document.createElement("input");
+        date.type = "date";
+        date.id = "todo-date";
+        date.required = true;
+        content.appendChild(date);
+
+        const projectLabel = document.createElement("label");
+        projectLabel.for = "todo-project";
+        projectLabel.textContent = "Add to project: ";
+        content.appendChild(projectLabel);
+
+        const projectSelect = document.createElement("select");
+        projectSelect.id = "todo-project";
+        projectSelect.required = true;
+
+        app.projects.forEach(project => {
+            let option = document.createElement("option");
+            option.value = project;
+            option.textContent = project;
+            projectSelect.appendChild(option);
+        });
+
+        content.appendChild(projectSelect);
+
+        const priorityLabel = document.createElement("label");
+        priorityLabel.for = "todo-priority";
+        priorityLabel.textContent = "Add priority level: ";
+        content.appendChild(priorityLabel);
+
+        const priority = document.createElement("select");
+        priority.id = "todo-priority";
+        priority.required = true;
+
+        const priority1 = document.createElement("option");
+        priority1.value = "normal";
+        priority1.textContent = "Normal";
+        priority.appendChild(priority1);
+
+        const priority2 = document.createElement("option");
+        priority2.value = "med";
+        priority2.textContent = "Medium";
+        priority.appendChild(priority2);
+
+        const priority3 = document.createElement("option");
+        priority3.value = "high";
+        priority3.textContent = "High";
+        priority.appendChild(priority3);
+
+        content.appendChild(priority);
+
+        const buttonContainer = document.createElement("div");
+        buttonContainer.classList.add("todo-popup-button-container");
+        editTodoPopup.appendChild(buttonContainer);
+
+        const save = document.createElement("button");
+        save.classList.add("todo-popup-save");
+        save.textContent = "Save";
+        buttonContainer.appendChild(save);
+
+        // Publish "Add new todo" event
+        // save.addEventListener("click", () => {
+        //     events.emit("User inputs new todo", [title.value, description.value, date.value, projectSelect.options[projectSelect.selectedIndex].value, priority.options[priority.selectedIndex].value]);
+        // })
+
+        const cancel = document.createElement("button");
+        cancel.textContent = "Cancel";
+        buttonContainer.appendChild(cancel);
+
+        // Cancel button closes popup
+        cancel.addEventListener("click", removeEditTodoPopup);
+
+        // Create transparent overlay behind popup
+        const overlay = document.createElement("div");
+        overlay.id = "overlay";
+
+        // If new todo successfully added, close popup
+        events.on("todos changed", removeEditTodoPopup);
+
+        body.appendChild(editTodoPopup);
+        body.appendChild(overlay);
         e.stopPropagation();
+    }
+
+    function removeEditTodoPopup() {
+        const body = document.querySelector("body");
+        const editTodoPopup = document.querySelector(".todo-popup");
+        const overlay = document.querySelector("#overlay");
+        console.log(body.removeChild(editTodoPopup));
+        console.log(body.removeChild(overlay));
     }
 
     return { displayEditTodo };
