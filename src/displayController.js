0817f52f07dc40a84914ab4dd49b9f5f82a27416
Jason <jhphan1@gmail.com>
2022-03-31 14:45:12 -0700

    Consolidate code into 1 renderTodos function that is usable for each display

--- src/displayController.js
+++ src/displayController.js
@@ -3,6 +3,114 @@ import app from "./index";
 import { editTodoPopup } from "./popups";
 import { format, parseISO, parse } from 'date-fns';
 
+// Renders list of todos onto page using todos array
+function renderTodos(todosArray) {
+    // Sort todos array by due date (ascending order)
+    todosArray.sort((a, b) => {
+        if (a.dueDate < b.dueDate) return -1;
+        if (a.dueDate > b.dueDate) return 1;
+        if (a.dueDate == b.dueDate) return 0;
+    })
+
+    // Sort completed todos to the bottom of list
+    todosArray.sort((a, b) => {
+        if (a.completed && !b.completed) return 1;
+        if (!a.completed && b.completed) return -1;
+        if (a.completed && b.completed || !a.completed && !b.completed) return 0;
+    })
+
+    todosArray.forEach(todo => {
+        const todoContainer = document.createElement("div");
+        // Change appearance of todo based on 'completed' property
+        if (todo.completed === false) {
+            todoContainer.classList.add("todo");
+        } else {
+            todoContainer.classList.add("todo-completed");
+        }
+
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        // Render with checked box if todo completed
+        checkbox.checked = (todo.completed) ? true : false;
+        // Tell app when user completes/un-completes todo
+        checkbox.addEventListener("change", () => {
+            if (checkbox.checked) {
+                events.emit("User completes todo", todoTitle.textContent);
+            } else {
+                events.emit("User un-completes todo", todoTitle.textContent);
+            }
+        })
+        // Clicking checkbox doesn't expand todo
+        checkbox.addEventListener("click", e => e.stopPropagation());
+        
+        todoContainer.appendChild(checkbox);
+
+        const todoTitle = document.createElement("span");
+        todoTitle.textContent = todo.title;
+        todoContainer.appendChild(todoTitle);
+
+        const dueDate = document.createElement("span");
+        dueDate.classList.add("due-date");
+        dueDate.textContent = todo.dueDate;
+        todoContainer.appendChild(dueDate);
+
+        const projectLabel = document.createElement("span");
+        projectLabel.classList.add("project-label");
+        projectLabel.textContent = todo.project;
+        todoContainer.appendChild(projectLabel);
+
+        const priority = document.createElement("span");
+        if (todo.priority === "normal") {
+            priority.classList.add("priority-normal");
+        } else if (todo.priority === "medium") {
+            priority.classList.add("priority-med");
+        } else if (todo.priority === "high") {
+            priority.classList.add("priority-high");
+        }
+        todoContainer.appendChild(priority);
+
+        main.appendChild(todoContainer);
+
+        // Hidden elements that expand when todo is clicked
+        todoContainer.addEventListener("click", () => {
+            // Toggle on/off
+            if (todoContainer.childNodes.length > 5) {
+                todoContainer.removeChild(todoContainer.lastChild);
+            } else {
+                const todoExpanded = document.createElement("div");
+                todoExpanded.classList.add("todo-expanded");
+                todoContainer.appendChild(todoExpanded);
+
+                const description = document.createElement("div");
+                description.textContent = todo.description;
+                todoExpanded.appendChild(description);
+
+                const buttonContainer = document.createElement("div");
+                buttonContainer.classList.add("expanded-todo-button-container");
+                todoExpanded.appendChild(buttonContainer);
+
+                const edit = document.createElement("button");
+                edit.id = "edit-todo-button";
+                edit.textContent = "Edit";
+                buttonContainer.appendChild(edit);
+
+                const trash = document.createElement("button");
+                trash.id = "trash-todo-button";
+                trash.textContent = "Delete";
+                buttonContainer.appendChild(trash);
+
+                trash.addEventListener("click", (e) => {
+                    let targetObject = e.target.parentNode.parentNode.parentNode.children[1].textContent;
+                    events.emit("User deletes todo", targetObject);
+                });
+
+                edit.addEventListener("click", editTodoPopup.displayEditTodo);
+            }
+        });
+    })
+}
+
+// Renders all todos onto page
 const renderAllTasks = (() => {
     // events.on("todos changed", render);
 
@@ -20,110 +128,8 @@ const renderAllTasks = (() => {
         title.textContent = "All Tasks";
         main.appendChild(title);
 
-        // Sort todos array by due date (ascending order)
-        todos.sort((a, b) => {
-            if (a.dueDate < b.dueDate) return -1;
-            if (a.dueDate > b.dueDate) return 1;
-            if (a.dueDate == b.dueDate) return 0;
-        })
-
-        // Sort completed todos to the bottom of list
-        todos.sort((a, b) => {
-            if (a.completed && !b.completed) return 1;
-            if (!a.completed && b.completed) return -1;
-            if (a.completed && b.completed || !a.completed && !b.completed) return 0;
-        })
-
         // Render new todos list
-        todos.forEach(todo => {
-            const todoContainer = document.createElement("div");
-            // Change appearance of todo based on 'completed' property
-            if (todo.completed === false) {
-                todoContainer.classList.add("todo");
-            } else {
-                todoContainer.classList.add("todo-completed");
-            }
-
-            const checkbox = document.createElement("input");
-            checkbox.type = "checkbox";
-            // Render with checked box if todo completed
-            checkbox.checked = (todo.completed) ? true : false;
-            // Tell app when user completes/un-completes todo
-            checkbox.addEventListener("change", () => {
-                if (checkbox.checked) {
-                    events.emit("User completes todo", todoTitle.textContent);
-                } else {
-                    events.emit("User un-completes todo", todoTitle.textContent);
-                }
-            })
-            // Clicking checkbox doesn't expand todo
-            checkbox.addEventListener("click", e => e.stopPropagation());
-            
-            todoContainer.appendChild(checkbox);
-
-            const todoTitle = document.createElement("span");
-            todoTitle.textContent = todo.title;
-            todoContainer.appendChild(todoTitle);
-
-            const dueDate = document.createElement("span");
-            dueDate.classList.add("due-date");
-            dueDate.textContent = todo.dueDate;
-            todoContainer.appendChild(dueDate);
-
-            const projectLabel = document.createElement("span");
-            projectLabel.classList.add("project-label");
-            projectLabel.textContent = todo.project;
-            todoContainer.appendChild(projectLabel);
-
-            const priority = document.createElement("span");
-            if (todo.priority === "normal") {
-                priority.classList.add("priority-normal");
-            } else if (todo.priority === "medium") {
-                priority.classList.add("priority-med");
-            } else if (todo.priority === "high") {
-                priority.classList.add("priority-high");
-            }
-            todoContainer.appendChild(priority);
-
-            main.appendChild(todoContainer);
-
-            // Hidden elements that expand when todo is clicked
-            todoContainer.addEventListener("click", () => {
-                // Toggle on/off
-                if (todoContainer.childNodes.length > 5) {
-                    todoContainer.removeChild(todoContainer.lastChild);
-                } else {
-                    const todoExpanded = document.createElement("div");
-                    todoExpanded.classList.add("todo-expanded");
-                    todoContainer.appendChild(todoExpanded);
-
-                    const description = document.createElement("div");
-                    description.textContent = todo.description;
-                    todoExpanded.appendChild(description);
-
-                    const buttonContainer = document.createElement("div");
-                    buttonContainer.classList.add("expanded-todo-button-container");
-                    todoExpanded.appendChild(buttonContainer);
-
-                    const edit = document.createElement("button");
-                    edit.id = "edit-todo-button";
-                    edit.textContent = "Edit";
-                    buttonContainer.appendChild(edit);
-
-                    const trash = document.createElement("button");
-                    trash.id = "trash-todo-button";
-                    trash.textContent = "Delete";
-                    buttonContainer.appendChild(trash);
-
-                    trash.addEventListener("click", (e) => {
-                        let targetObject = e.target.parentNode.parentNode.parentNode.children[1].textContent;
-                        events.emit("User deletes todo", targetObject);
-                    });
-
-                    edit.addEventListener("click", editTodoPopup.displayEditTodo);
-                }
-            });
-        })
+        renderTodos(todos);
     }
 })();
 
@@ -132,9 +138,6 @@ const renderToday = (() => {
 
     // Find today's date
     let today = format((new Date()), 'MM/dd/yy');
-
-    let someDate = format(parse("12/14/92", "MM/dd/yy", new Date()), 'yyyy-MM-dd');
-    console.log(someDate);
 
     function render(todos) {
         const main = document.querySelector("#main");
@@ -153,103 +156,8 @@ const renderToday = (() => {
         // Filter for only todos due today
         let todaysTodos = todos.filter(todo => todo.dueDate === today);
 
-        // Sort completed todos to the bottom of list
-        todaysTodos.sort((a, b) => {
-            if (a.completed && !b.completed) return 1;
-            if (!a.completed && b.completed) return -1;
-            if (a.completed && b.completed || !a.completed && !b.completed) return 0;
-        })
-
         // Render new todos list
-        todaysTodos.forEach(todo => {
-            const todoContainer = document.createElement("div");
-            // Change appearance of todo based on 'completed' property
-            if (todo.completed === false) {
-                todoContainer.classList.add("todo");
-            } else {
-                todoContainer.classList.add("todo-completed");
-            }
-
-            const checkbox = document.createElement("input");
-            checkbox.type = "checkbox";
-            // Render with checked box if todo completed
-            checkbox.checked = (todo.completed) ? true : false;
-            // Tell app when user completes/un-completes todo
-            checkbox.addEventListener("change", () => {
-                if (checkbox.checked) {
-                    events.emit("User completes todo", todoTitle.textContent);
-                } else {
-                    events.emit("User un-completes todo", todoTitle.textContent);
-                }
-            })
-            // Clicking checkbox doesn't expand todo
-            checkbox.addEventListener("click", e => e.stopPropagation());
-            
-            todoContainer.appendChild(checkbox);
-
-            const todoTitle = document.createElement("span");
-            todoTitle.textContent = todo.title;
-            todoContainer.appendChild(todoTitle);
-
-            const dueDate = document.createElement("span");
-            dueDate.classList.add("due-date");
-            dueDate.textContent = todo.dueDate;
-            todoContainer.appendChild(dueDate);
-
-            const projectLabel = document.createElement("span");
-            projectLabel.classList.add("project-label");
-            projectLabel.textContent = todo.project;
-            todoContainer.appendChild(projectLabel);
-
-            const priority = document.createElement("span");
-            if (todo.priority === "normal") {
-                priority.classList.add("priority-normal");
-            } else if (todo.priority === "medium") {
-                priority.classList.add("priority-med");
-            } else if (todo.priority === "high") {
-                priority.classList.add("priority-high");
-            }
-            todoContainer.appendChild(priority);
-
-            main.appendChild(todoContainer);
-
-            // Hidden elements that expand when todo is clicked
-            todoContainer.addEventListener("click", () => {
-                // Toggle on/off
-                if (todoContainer.childNodes.length > 5) {
-                    todoContainer.removeChild(todoContainer.lastChild);
-                } else {
-                    const todoExpanded = document.createElement("div");
-                    todoExpanded.classList.add("todo-expanded");
-                    todoContainer.appendChild(todoExpanded);
-
-                    const description = document.createElement("div");
-                    description.textContent = todo.description;
-                    todoExpanded.appendChild(description);
-
-                    const buttonContainer = document.createElement("div");
-                    buttonContainer.classList.add("expanded-todo-button-container");
-                    todoExpanded.appendChild(buttonContainer);
-
-                    const edit = document.createElement("button");
-                    edit.id = "edit-todo-button";
-                    edit.textContent = "Edit";
-                    buttonContainer.appendChild(edit);
-
-                    const trash = document.createElement("button");
-                    trash.id = "trash-todo-button";
-                    trash.textContent = "Delete";
-                    buttonContainer.appendChild(trash);
-
-                    trash.addEventListener("click", (e) => {
-                        let targetObject = e.target.parentNode.parentNode.parentNode.children[1].textContent;
-                        events.emit("User deletes todo", targetObject);
-                    });
-
-                    edit.addEventListener("click", editTodoPopup.displayEditTodo);
-                }
-            });
-        })
+        renderTodos(todaysTodos);
     }
 })();
 
